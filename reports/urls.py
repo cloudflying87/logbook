746e81359404178570bf6682495720e7f915410b
David Hale <davidhale87@gmail.com>
2022-09-13 15:33:27 -0500

    Working on reports page. Added totals for categories, Added lookup for airports

--- reports/urls.py
+++ reports/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from reports.views import Totals, TotalsOnly, CategoryDisplay
+from reports.views import Totals, TotalsOnly, CategoryDisplay, Lookup, AirportLookup
 
 
 urlpatterns = [
     path('', Totals.as_view(), name="reporttotals"),
     path('totals', TotalsOnly.as_view(), name="totals"),
     path('category', CategoryDisplay.as_view(), name="category"),
+    path('lookup', Lookup.as_view(), name="lookup"),
+    path('airportlookup', AirportLookup.as_view(), name="airportlookup"),
     
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,7 @@
 
+from cgitb import lookup
+from codecs import lookup_error
+import re
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
@@ -41,12 +44,33 @@ class TotalsOnly(TemplateView):
                 iap= Sum('iap'),
                 holdnumber= Sum('holdnumber'),
                 crosscountry= Sum('crosscountry'),
-                pax = Sum('passengercount')
+                pax = Sum('passengercount'),
+                miles = Sum('distance')
             ),
 
             'totalflights': FlightTime.objects.all().filter(userid=userid).exclude(Q(scheduledflight=1) | Q(total=0)).aggregate(
             tflight = Count('flightdate'))}
         return self.render_to_response({"qs":qs, "title":pagetitle})
+
+class AirportLookup(TemplateView):
+    template_name = 'reports/airportreport.html'
+    def get(self, request, *args, **kwargs):
+        pagetitle = "Airports"
+        userid = getuserid()
+        secondrequest = request.GET.get('lookup[id]')
+        depart = FlightTime.objects.filter(Q(departure=secondrequest) | Q(arrival=secondrequest),userid=userid).exclude(Q(scheduledflight=1) | Q(total=0)).order_by('-flightdate')
+        return self.render_to_response({'total':depart.count(),'depart':depart,"title":pagetitle})
+
+class Lookup(TemplateView):
+    template_name = 'reports/lookup.html'
+    def get(self, request, *args, **kwargs):
+        pagetitle = "Look-Up"
+        lookuprequest = request.GET.get('option')
+        secondrequest = request.GET.get('lookup[id]')
+
+        if lookuprequest == 'airport':
+            print('airport',secondrequest)
+        return self.render_to_response({"title":pagetitle})
 
 class CategoryDisplay(TemplateView):
     template_name = 'reports/categorydisplay.html'
@@ -54,29 +78,52 @@ class CategoryDisplay(TemplateView):
         pagetitle = "Category Report"
         qs = []
         userid = getuserid()
-        
-        manufacture = Manufacture.objects.all()
+        manufacture = Manufacture.objects.all().values()
+        #this is used to get the category totals, you can also select the manufacture totals dropdown
         if request.GET.get('category') != None:
-            id = Manufacture.objects.get(manufacture=request.GET.get('category')).pk
-            print(id)
+            id = request.GET.get('category')
             uniquetype = AircraftModel.objects.filter(mfr=id)
-            print(uniquetype)
             for airtype in uniquetype:
-                print(airtype)
-                info = {'aircraftsummary': FlightTime.objects.filter(userid=userid,aircrafttype=airtype).aggregate(
-                    airtotal = Sum('total'),
-                    avgflight = Avg('total'),
-                    numflight = Count('total'),
-                    avgmiles = Round(Avg('distance')),
-                    totalmiles = Sum('distance'),
-                    avgpax = Round(Avg('passengercount')),
-                    totalpax = Sum('passengercount')
-                )}
-                record = {'aircraftype':airtype,'info':info}
-                if record['info']['aircraftsummary']['airtotal'] != None:
-                    qs.append(record)   
+                checkingfornone = FlightTime.objects.filter(userid=userid,aircrafttype=airtype).aggregate(Sum('total'))
+                
+                if checkingfornone['total__sum'] != None:
+                    info = {'aircraftsummary': FlightTime.objects.filter(userid=userid,aircrafttype=airtype).aggregate(
+                        airtotal = Sum('total'),
+                        avgflight = Avg('total'),
+                        numflight = Count('total'),
+                        avgmiles = Avg('distance'),
+                        totalmiles = Sum('distance'),
+                        avgpax = Avg('passengercount'),
+                        totalpax = Sum('passengercount'),
+                        night = Sum('night'),
+                        landings = Sum('landings'),
+                    )}
+                    #making it so that i can round the results the way I want
+                    #them before appending them to the list to send them to the
+                    #template in the context
+                    record = {'aircraftype':airtype,'info':{'aircraftsummary':{'airtotal':info['aircraftsummary']['airtotal'],'avgflight':round((info['aircraftsummary']['avgflight']),2),'numflight':info['aircraftsummary']['numflight'],'avgmiles':int(round((info['aircraftsummary']['avgmiles']),0)),'totalmiles':info['aircraftsummary']['totalmiles'],'avgpax':int(round((info['aircraftsummary']['avgpax']),0)),'totalpax':info['aircraftsummary']['totalpax'],'night':info['aircraftsummary']['night'],'landings':info['aircraftsummary']['landings']}}}
+                    if record['info']['aircraftsummary']['airtotal'] != None:
+                        qs.append(record)
+            tairtotal = 0; tavgflight = 0; tnumflight = 0; tavgmiles = 0
+            ttotalmiles = 0; tavgpax = 0; ttotalpax = 0; count = 0; tnight = 0; tlandings = 0;
+            #adding the totals for the categories
+            for itemtotal in qs:
+                tairtotal = tairtotal + (itemtotal['info']['aircraftsummary']['airtotal'])  
+                tavgflight = tavgflight + (itemtotal['info']['aircraftsummary']['avgflight'])  
+                tnumflight = tnumflight + (itemtotal['info']['aircraftsummary']['numflight'])  
+                tavgmiles = tavgmiles + (itemtotal['info']['aircraftsummary']['avgmiles'])  
+                ttotalmiles = ttotalmiles + (itemtotal['info']['aircraftsummary']['totalmiles'])  
+                tavgpax = tavgpax + (itemtotal['info']['aircraftsummary']['avgpax'])  
+                ttotalpax = ttotalpax + (itemtotal['info']['aircraftsummary']['totalpax'])  
+                tnight = tnight + (itemtotal['info']['aircraftsummary']['night'])  
+                tlandings = tlandings + (itemtotal['info']['aircraftsummary']['landings'])  
+                count += 1
+            
+            record = {'aircraftype':'Total','info':{'aircraftsummary':{'airtotal':tairtotal,'avgflight':round((tavgflight/count),2),'numflight':tnumflight,'avgmiles':int((tavgmiles/count)),'totalmiles':ttotalmiles,'avgpax':int(tavgpax/count),'totalpax':ttotalpax,'night':tnight,'landings':tlandings}}}
+            qs.append(record) 
             return self.render_to_response({"manufacture":manufacture,"qs":qs, "title":pagetitle})
         else:
+            #gets all the airplanes
             uniquetype = FlightTime.objects.filter(userid=userid).values('aircrafttype').distinct()
             for airtype in uniquetype:
                 info = {'aircraftsummary': FlightTime.objects.filter(userid=userid,aircrafttype=airtype['aircrafttype']).aggregate(
@@ -86,9 +133,11 @@ class CategoryDisplay(TemplateView):
                     avgmiles = Round(Avg('distance')),
                     totalmiles = Sum('distance'),
                     avgpax = Round(Avg('passengercount')),
-                    totalpax = Sum('passengercount')
+                    totalpax = Sum('passengercount'),
+                    night = Sum('night'),
+                    landings = Sum('landings'),
                 )}
-                record = {'aircraftype':airtype['aircrafttype'],'info':info}
+                record = {'aircraftype':airtype['aircrafttype'],'info':{'aircraftsummary':{'airtotal':info['aircraftsummary']['airtotal'],'avgflight':round((info['aircraftsummary']['avgflight']),2),'numflight':info['aircraftsummary']['numflight'],'avgmiles':int(round((info['aircraftsummary']['avgmiles']),0)),'totalmiles':info['aircraftsummary']['totalmiles'],'avgpax':int(round((info['aircraftsummary']['avgpax']),0)),'totalpax':info['aircraftsummary']['totalpax'],'night':info['aircraftsummary']['night'],'landings':info['aircraftsummary']['landings']}}}
                 
                 if record['info']['aircraftsummary']['airtotal'] > 0:
                     qs.append(record)
